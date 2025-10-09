57b7cbc790c634296861886f1a9f421278d67d88
Kwame Twum <kwame.m.twum@gmail.com>
2025-10-09 22:02:21 +0000

    Add support for MPS device and enhance facemodel selection logic in inference

--- src/gradio_demo.py
+++ src/gradio_demo.py
@@ -1,7 +1,8 @@
 import torch, uuid
-import os, sys, shutil
+import os, sys, shutil, platform
+from src.facerender.pirender_animate import AnimateFromCoeff_PIRender
 from src.utils.preprocess import CropAndExtract
-from src.test_audio2coeff import Audio2Coeff  
+from src.test_audio2coeff import Audio2Coeff
 from src.facerender.animate import AnimateFromCoeff
 from src.generate_batch import get_data
 from src.generate_facerender_batch import get_facerender_data
@@ -22,20 +23,24 @@ class SadTalker():
 
         if torch.cuda.is_available() :
             device = "cuda"
+        elif platform.system() == 'Darwin': # macos
+            device = "mps"
         else:
             device = "cpu"
-        
+
         self.device = device
 
         os.environ['TORCH_HOME']= checkpoint_path
 
         self.checkpoint_path = checkpoint_path
         self.config_path = config_path
-      
 
-    def test(self, source_image, driven_audio, preprocess='crop', 
-        still_mode=False,  use_enhancer=False, batch_size=1, size=256, 
-        pose_style = 0, exp_scale=1.0, 
+
+    def test(self, source_image, driven_audio, preprocess='crop',
+        still_mode=False,  use_enhancer=False, batch_size=1, size=256,
+        pose_style = 0,
+        facerender='facevid2vid',
+        exp_scale=1.0,
         use_ref_video = False,
         ref_video = None,
         ref_info = None,
@@ -45,10 +50,19 @@ class SadTalker():
 
         self.sadtalker_paths = init_path(self.checkpoint_path, self.config_path, size, False, preprocess)
         print(self.sadtalker_paths)
-            
+
         self.audio_to_coeff = Audio2Coeff(self.sadtalker_paths, self.device)
         self.preprocess_model = CropAndExtract(self.sadtalker_paths, self.device)
-        self.animate_from_coeff = AnimateFromCoeff(self.sadtalker_paths, self.device)
+        # self.animate_from_coeff = AnimateFromCoeff(self.sadtalker_paths, self.device)
+
+        if facerender == 'facevid2vid' and self.device != 'mps':
+            self.animate_from_coeff = AnimateFromCoeff(self.sadtalker_paths, self.device)
+        elif facerender == 'pirender' or self.device == 'mps':
+            self.animate_from_coeff = AnimateFromCoeff_PIRender(self.sadtalker_paths, self.device)
+            facerender = 'pirender'
+        else:
+            raise(RuntimeError('Unknown model: {}'.format(facerender)))
+
 
         time_tag = str(uuid.uuid4())
         save_dir = os.path.join(result_dir, time_tag)
@@ -58,11 +72,11 @@ class SadTalker():
         os.makedirs(input_dir, exist_ok=True)
 
         print(source_image)
-        pic_path = os.path.join(input_dir, os.path.basename(source_image)) 
+        pic_path = os.path.join(input_dir, os.path.basename(source_image))
         shutil.move(source_image, input_dir)
 
         if driven_audio is not None and os.path.isfile(driven_audio):
-            audio_path = os.path.join(input_dir, os.path.basename(driven_audio))  
+            audio_path = os.path.join(input_dir, os.path.basename(driven_audio))
 
             #### mp3 to wav
             if '.mp3' in audio_path:
@@ -86,15 +100,15 @@ class SadTalker():
             print('new audiopath:',audio_path)
             # if ref_video contains audio, set the audio from ref_video.
             cmd = r"ffmpeg -y -hide_banner -loglevel error -i %s %s"%(ref_video, audio_path)
-            os.system(cmd)        
+            os.system(cmd)
 
         os.makedirs(save_dir, exist_ok=True)
-        
+
         #crop image and extract 3dmm from image
         first_frame_dir = os.path.join(save_dir, 'first_frame_dir')
         os.makedirs(first_frame_dir, exist_ok=True)
         first_coeff_path, crop_pic_path, crop_info = self.preprocess_model.generate(pic_path, first_frame_dir, preprocess, True, size)
-        
+
         if first_coeff_path is None:
             raise AttributeError("No face is detected")
 
@@ -135,7 +149,7 @@ class SadTalker():
             coeff_path = self.audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
 
         #coeff2video
-        data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, batch_size, still_mode=still_mode, preprocess=preprocess, size=size, expression_scale = exp_scale)
+        data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, batch_size, still_mode=still_mode, preprocess=preprocess, size=size, expression_scale = exp_scale, facemodel=facerender)
         return_path = self.animate_from_coeff.generate(data, save_dir,  pic_path, crop_info, enhancer='gfpgan' if use_enhancer else None, preprocess=preprocess, img_size=size)
         video_name = data['video_name']
         print(f'The generated video is named {video_name} in {save_dir}')
@@ -147,9 +161,8 @@ class SadTalker():
         if torch.cuda.is_available():
             torch.cuda.empty_cache()
             torch.cuda.synchronize()
-            
+
         import gc; gc.collect()
-        
+
         return return_path
 
-    
