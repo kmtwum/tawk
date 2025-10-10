62344a1a67cb2c1bd950673913a9ef63c752679d
Kwame Twum <kwame.m.twum@gmail.com>
2025-10-10 13:02:03 +0000

    Update `save_video_with_watermark` to use `vcodec copy` for lossless video encoding

--- src/utils/videoio.py
+++ src/utils/videoio.py
@@ -19,6 +19,6 @@ def load_video_to_cv2(input_path):
 
 def save_video_with_watermark(video, audio, save_path):
     temp_file = str(uuid.uuid4())+'.mp4'
-    cmd = r'ffmpeg -y -hide_banner -loglevel error -i "%s" -i "%s" -vcodec mpeg4 "%s"' % (video, audio, temp_file)
+    cmd = r'ffmpeg -y -hide_banner -loglevel error -i "%s" -i "%s" -vcodec copy "%s"' % (video, audio, temp_file)
     os.system(cmd)
     shutil.move(temp_file, save_path)
