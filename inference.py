a3bc8ec1892a4f88a59ddda71685afe84376c05c
Kwame Twum <kwame.m.twum@gmail.com>
2025-10-10 10:49:23 +0000

    Refactor `facemodel` parameter to `face_model` for consistency across functions and scripts

--- inference.py
+++ inference.py
@@ -86,9 +86,9 @@ def main(args):
         gen_composed_video(args, device, first_coeff_path, coeff_path, audio_path, os.path.join(save_dir, '3dface.mp4'))
     
     #coeff2video
-    data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, 
-                                batch_size, input_yaw_list, input_pitch_list, input_roll_list,
-                                expression_scale=args.expression_scale, still_mode=args.still, preprocess=args.preprocess, size=args.size, facemodel=args.facerender)
+    data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path,
+                               batch_size, input_yaw_list, input_pitch_list, input_roll_list,
+                               expression_scale=args.expression_scale, still_mode=args.still, preprocess=args.preprocess, size=args.size, face_model=args.facerender)
     
     result = animate_from_coeff.generate(data, save_dir, pic_path, crop_info,
                                 enhancer=args.enhancer, background_enhancer=args.background_enhancer, preprocess=args.preprocess, img_size=args.size)
--- src/gradio_demo.py
+++ src/gradio_demo.py
@@ -149,7 +149,7 @@ class SadTalker():
             coeff_path = self.audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
 
         #coeff2video
-        data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, batch_size, still_mode=still_mode, preprocess=preprocess, size=size, expression_scale = exp_scale, facemodel=facerender)
+        data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, batch_size, still_mode=still_mode, preprocess=preprocess, size=size, expression_scale = exp_scale, face_model=facerender)
         return_path = self.animate_from_coeff.generate(data, save_dir,  pic_path, crop_info, enhancer='gfpgan' if use_enhancer else None, preprocess=preprocess, img_size=size)
         video_name = data['video_name']
         print(f'The generated video is named {video_name} in {save_dir}')
